e68bd646163f726ff13bf7ded6d8995c2139ecea
ISWARYA107 <iswaryas504@gmail.com>
2026-06-15 14:29:59 +0530

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>KONGU ENGINNERING COLLEGE</h1>
+          <h1>Iswarya s</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
